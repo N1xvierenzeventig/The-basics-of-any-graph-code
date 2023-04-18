f6f26c88e216000ee531316761f3cb3083f04a7c
N1xvierenzeventig <111287243+N1xvierenzeventig@users.noreply.github.com>
2023-04-18 17:09:55 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 x = []
 y = []
-x2 = np.arange(0,100,0.5)
+x2 = np.arange(0,200,0.5)
 plt.plot(x2[:100], x2[:100]*x2[:100], "r", label="X**X")
 plt.plot(x2[100:], x2[100:]*x2[100:], "b--")
-
-print(x2)
 f = [i*10 for i in range(11)]
 f2 = [i*1000 for i in range(11)]
 x_axis = 0
@@ -16,10 +14,6 @@ for i in range(100):
     x.append(x_axis)
     y.append(y_axis)
     x_axis += 1
-
-
-print(x)
-print(y)
 plt.plot(x,y, "g.--", label="2x")
 plt.title("Random Graph", fontdict={"fontname": "Garamond", "fontsize":20})
 plt.xlabel("X", fontdict={"fontname": "Garamond", "fontsize":10})
