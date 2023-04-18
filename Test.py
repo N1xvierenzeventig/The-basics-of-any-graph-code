de1fc46ea56c404eddcd0e1070bddf3de758ed34
N1xvierenzeventig <111287243+N1xvierenzeventig@users.noreply.github.com>
2023-04-18 17:08:54 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,12 +1,18 @@
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 x = []
 y = []
+x2 = np.arange(0,100,0.5)
+plt.plot(x2[:100], x2[:100]*x2[:100], "r", label="X**X")
+plt.plot(x2[100:], x2[100:]*x2[100:], "b--")
+
+print(x2)
 f = [i*10 for i in range(11)]
-f2 = [i*20 for i in range(11)]
+f2 = [i*1000 for i in range(11)]
 x_axis = 0
 for i in range(100):
-    y_axis = random.randint(1,200)
+    y_axis = random.randint(1,10001)
     x.append(x_axis)
     y.append(y_axis)
     x_axis += 1
